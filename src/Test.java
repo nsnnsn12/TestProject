3d2c02c89dbcfa7a33989fc30fac8bd66da8d73b
COM <COM@DESKTOP-TH36TT5>
2020-11-17 16:17:14 +0900

    test6

--- src/Test.java
+++ src/Test.java
@@ -2,9 +2,8 @@
 public class Test {
 	//content1
 	//content2
-<<<<<<< HEAD
-	//content4
-=======
 	//content3
->>>>>>> branch 'master' of https://github.com/nsnnsn12/TestProject.git
+	//content4
+
+	
 }
